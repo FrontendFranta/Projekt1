16651ab1c1c805964cc7050e89c7c7a7b6d3ffc2
Zloprcek <frantafajna@gmail.com>
2023-03-03 21:07:12 +0100

    message

--- src/components/Sata.js
+++ src/components/Sata.js
@@ -12,47 +12,41 @@ const sata = [
         img: img1,
         category: "Skyscraper",
         name: "Big Skyscraper",
-        price: 400,
-        text: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia,"
+      
     },
     {
         _id: 2,
         img: img2,
         category: "Villa",
         name: "Modern Villa",
-        price: 700,
-        text: "Obchodujte s ním a dávejte mu Emeraldy"
+      
     },
     {
         _id: 3,
         img: img3,
         category: "Skyscraper",
         name: "Family Skyscraper",
-        price: 1000,
-        text: "2 metrová skála co tě zabije, když jí utrhneš kamínek"
+        
     },
     {
         _id: 4,
         img: img4,
         category: "House",
         name: "Modern House",
-        price: 7000,
-        text: "Miluje když ho negr ducá doprdele"
+       
     },
     {
         _id: 5,
         img: img5,
         category: "House",
         name: "Modern Roof",
-        price: 8000,
-        text: "Anal"
+        
     },{
         _id: 6,
         img: img6,
         category: "Villa",
         name: "Glass Villa",
-        price: 8000,
-        text: "Anal"
+        
     }
 
     
